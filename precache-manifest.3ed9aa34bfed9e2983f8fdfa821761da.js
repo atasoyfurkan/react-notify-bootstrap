436de0287c845b9985c3e70216246b2e9c8ac365
atasoyfurkan <=>
2020-05-02 03:52:29 +0300

    Updates

---
+++ precache-manifest.3ed9aa34bfed9e2983f8fdfa821761da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6e06d4ec240bf0ce56734fce383c05",
+    "url": "/react-notify-bootstrap/index.html"
+  },
+  {
+    "revision": "d67e3256c94464e5c881",
+    "url": "/react-notify-bootstrap/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "69c2978d84bce5cba629",
+    "url": "/react-notify-bootstrap/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d67e3256c94464e5c881",
+    "url": "/react-notify-bootstrap/static/js/2.23e02a1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-notify-bootstrap/static/js/2.23e02a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c2978d84bce5cba629",
+    "url": "/react-notify-bootstrap/static/js/main.20f77afe.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/react-notify-bootstrap/static/js/main.20f77afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c5b05dfc386dbae58e",
+    "url": "/react-notify-bootstrap/static/js/runtime-main.eb27641c.js"
+  }
+]);
